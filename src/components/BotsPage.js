0d2c0725e9c8cb04f4fae5910949754134f4d5c6
SUNDUS-THE-CODER <sundusahmed648@gmail.com>
2023-03-10 13:36:37 +0000

    fetch bots

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
-  //start here with your code for step one
+  const [bots, setBots] = useState([]);
 
+  const botsToDisplay = bots.sort((a, b) => b.created_at - a.created_at);
+  useEffect(() => {
+    const fetchBots = async () => {
+      try {
+        let fetchedBots = await fetch("http://localhost:8002/bots");
+        fetchedBots = await fetchedBots.json();
+        setBots(fetchedBots);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    fetchBots();
+  }, [])
   return (
     <div>
       <YourBotArmy />
-      <BotCollection />
+      <BotCollection bots={botsToDisplay} />
     </div>
   )
 }
